9697a938dcd959043a98dc8247a4c9f0bb953c08
Guilherme Ferrari <guiferrari10@hotmail.com>
2022-08-10 15:48:23 -0300

    feat: created summary component and modal component with radix

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,45 @@
-import { Container } from './styles'
+import {
+  Container,
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+
+import { SearchForm } from './components/SearchForm'
 
 export function Transactions() {
   return (
     <Container>
       <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td>Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 39,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </Container>
   )
 }
