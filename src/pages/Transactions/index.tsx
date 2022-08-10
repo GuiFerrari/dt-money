56916b70026e52804f487adbc93c405461a5502f
Guilherme Ferrari <guiferrari10@hotmail.com>
2022-08-10 14:46:58 -0300

    feat: created project with styled components

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles'
+
+import { Header } from '../../components/Header'
+
+export function Transactions() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  )
+}
